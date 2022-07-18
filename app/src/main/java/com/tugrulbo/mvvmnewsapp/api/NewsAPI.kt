46a0093c23cde81c8c89bba9dad874c02cae8fda
tugrulb <tugrulbo@gmail.com>
2022-07-18 06:56:58 +0300

    Room Setup
    Room Setup done

--- app/src/main/java/com/tugrulbo/mvvmnewsapp/api/NewsAPI.kt
+++ app/src/main/java/com/tugrulbo/mvvmnewsapp/api/NewsAPI.kt
@@ -1,6 +1,6 @@
 package com.tugrulbo.mvvmnewsapp.api
 
-import com.tugrulbo.mvvmnewsapp.ui.fragments.NewsResponse
+import com.tugrulbo.mvvmnewsapp.models.NewsResponse
 import com.tugrulbo.mvvmnewsapp.util.Constants.API_KEY
 import retrofit2.Response
 import retrofit2.http.GET
--- app/src/main/java/com/tugrulbo/mvvmnewsapp/models/Article.kt
+++ app/src/main/java/com/tugrulbo/mvvmnewsapp/models/Article.kt
@@ -1,9 +1,17 @@
-package com.tugrulbo.mvvmnewsapp.ui.fragments
+package com.tugrulbo.mvvmnewsapp.models
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id:Int? = null,
     @SerializedName("author")
     var author: String,
     @SerializedName("content")
